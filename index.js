9f84093d8d29e5eb0c6f544f24c66e3b35d328fe
Renan Victor Cassoli <renancassoli423@gmail.com>
2026-03-29 18:03:34 -0300

    atualizando site

--- index.js
+++ index.js
@@ -1,71 +1,66 @@
-//simulação para adicionar itens a encomenda
-//seleciona todos os botoes que possuem classe 
-// "categoria"
+// seleciona todos os botoes
 const botoesEscolher = document.querySelectorAll('.escolher');
 
-//seleciona a lista onde as encomendas serao exibidas
-const listaEncomenda = document.getElementById ('lista-Encomenda');
+// lista de encomenda
+const listaEncomenda = document.getElementById('lista-Encomenda');
 
-//seleciona o elemento que exibira o valor total da encomenda
+// elementos de exibição
 const totalElemento = document.getElementById('total');
+const descontoElemento = document.getElementById('desconto');
+const totalFinalElemento = document.getElementById('total-final');
 
-const descontoElemento = document.getElementById("desconto")
-
-//cria a variavel que armazena o valor total da encomenda
-let total = 0;
+// variáveis
 let quantidade = 0;
 let desconto = 0;
 
-//percorre todos os botoes com class "adicionar"
-//e adiciona um evento de click em cada um deles
-
+// clique nos produtos
 botoesEscolher.forEach((botao) => {
-    botao.addEventListener("click", () =>{
-        
-        descontoElemento.textContent =  `Desconto: R$ ${desconto.toFixed(2)}`;
+    botao.addEventListener("click", () => {
 
-        //obtem o elemento pai do botao (card do produto)
         const produto = botao.parentElement;
 
-        //obtem o nome o peidurto a partir do texto da tag <h2>
-        const  nome = produto.querySelector("h2").textContent;
+        const nome = produto.querySelector("h2").textContent;
 
-        //obtem o preco do produto, removendo o texto 'R$
-        const preco = parseFloat(produto.querySelector('.preco').textContent.replace("R$", "").replace(",", "."));
+        const preco = parseFloat(
+            produto.querySelector('.preco').textContent
+                .replace("R$", "")
+                .replace(",", ".")
+        );
 
-        //cria um item de lista para adicionar o produto a encomenda
         const itemEncomenda = document.createElement('li');
         itemEncomenda.textContent = `${nome} - R$ ${preco.toFixed(2)}`;
-
-        //adiciona o item criado a lista de encomendas
         listaEncomenda.appendChild(itemEncomenda);
 
-        //atualiza o total da compra somando o preço do novo item
-        quantidade ++;
+        // quantidade de itens
+        quantidade++;
 
         // total sem desconto
         let totalSemDesconto = quantidade * 15;
 
-        //calcula desconto (a cada 2 itens ganha 5)
+        // desconto (a cada 2 itens ganha 5)
         desconto = Math.floor(quantidade / 2) * 5;
 
-        //total final
-        total = totalSemDesconto - desconto;
+        // total final
+        let totalFinal = totalSemDesconto - desconto;
 
-
-
-        //atualiza o texto do elemento que exibe o total da encomenda
-        totalElemento.textContent = `Total: R$ ${total.toFixed(2)}`;
+        // atualiza tela
+        totalElemento.textContent = `Total: R$ ${totalSemDesconto.toFixed(2)}`;
+        descontoElemento.textContent = `Desconto: R$ ${desconto.toFixed(2)}`;
+        totalFinalElemento.textContent = `Agora: R$ ${totalFinal.toFixed(2)}`;
     });
-}); 
+});
 
+// botão finalizar
 const botaoFinalizarEncomenda = document.getElementById("Finalizar-Encomenda");
 
 botaoFinalizarEncomenda.addEventListener("click", () => {
-    alert("Encomenda Finalizada" + totalElemento.textContent);
-    listaEncomenda.innerHTML = "";
-    total = 0;
-    quantidade = 0;
-    totalElemento.textContent = `Total: R$ ${total.toFixed(2).replace(".", ",")}`; 
-});
+    alert("Encomenda Finalizada!");
 
+    listaEncomenda.innerHTML = "";
+    quantidade = 0;
+    desconto = 0;
+
+    totalElemento.textContent = "Total: R$ 0,00";
+    descontoElemento.textContent = "Desconto: R$ 0,00";
+    totalFinalElemento.textContent = "Agora: R$ 0,00";
+});             
